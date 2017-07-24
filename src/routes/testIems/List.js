51351403798d3d54cc7c404a50e443fb5cc725b0
MengStar <641510128@qq.com>
2017-07-24 21:03:52 +0800

    题库管理的展示

--- src/routes/testIems/List.js
+++ src/routes/testIems/List.js
@@ -1,35 +1,117 @@
 import React from 'react';
-import {Table} from 'antd';
+import {Table,Modal} from 'antd';
 import styles from './List.less';
+import { DropOption } from 'components'
+import PropTypes from 'prop-types'
+const confirm = Modal.confirm
+
 
 const List = ({...tableProps}) => {
-  const columns=[
-    {
-      title: 'Image',
-      dataIndex: 'image',
-      className: styles.image,
-      width: 64,
-      render: (text) => <img alt="Feture" width={26} src={text} />,
-    },{
-      title: '试题类型',
-      dataIndex: 'type',
-      render:(text)=> text === "QUESTION"?"选择题":"问答题",
+
+const {onEditItem,onDeleteItem}=tableProps
+  /**
+   * 选择题展示
+   * @param text
+   * @returns {XML}
+   */
+  const selectRowRender = (text) => {
+    const obj = JSON.parse(text);
+    console.log(obj);
+
+    return (
+      <div>
+        <div>题干：{obj.question}</div>
+        <div>
+          <div>
+            A:{obj.answer.A}
+          </div>
+          <div>
+            B:{obj.answer.B}
+          </div>
+          <div>
+            C:{obj.answer.C}
+          </div>
+          <div>
+            D:{obj.answer.D}
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+  /**
+   * 操作选项
+   * @param record
+   * @param e
+   */
+  const handleMenuClick = (record, e) => {
+    if (e.key === '1') {
+      onEditItem(record)
+    } else if (e.key === '2') {
+      confirm({
+        title: '你确定要删除这个用户吗?',
+        onOk () {
+          onDeleteItem(record.id)
+        },
+      })
+    }
+  }
+
+  const {type} = tableProps;
+  const columns = [
+ {
+      title: '试题分类',
+      dataIndex: 'subject',
+      render: (text) => text.type,
     },
-]
+    /**
+     * 判断是选择题还是问答题
+     * type=1 问答题
+     * 其他 选择题
+     */
+    type === 1 ?
+      {
+        title: '问题描述',
+        dataIndex: 'question',
+        render: (text) => text,
+      } :
+      {
+        title: '题目与选项',
+        dataIndex: 'question',
+        render: selectRowRender
+      },
+    {
+      title: '参考答案',
+      dataIndex: 'answer',
+      render: (text) => text,
+    },
+    {
+      title: '操作',
+      key: 'operation',
+      width: 100,
+      render: (text, record) => {
+        return <DropOption onMenuClick={e => handleMenuClick(record, e)} menuOptions={[{ key: '1', name: '修改' }, { key: '2', name: '删除' }]} />
+      },
+    },
+  ];
 
 
   return (
     <div>
       <Table
-        {...tableProps}
         bordered
+        simple
+        {...tableProps}
         scroll={{x: 800}}
         columns={columns}
-        simple
         className={styles.table}
         rowKey={record => record.id}
       />
     </div>
   );
 };
+List.propTypes = {
+  onDeleteItem: PropTypes.func,
+  onEditItem: PropTypes.func,
+}
 export default List;
--- src/routes/testIems/index.js
+++ src/routes/testIems/index.js
@@ -1,20 +1,39 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {routerRedux} from 'dva/router'
-import {connect} from 'dva'
-import List from './List'
+import React from 'react';
+import PropTypes from 'prop-types';
+import {routerRedux} from 'dva/router';
+import {connect} from 'dva';
+import List from './List';
+import {Tabs} from 'antd';
+
+const TabPane = Tabs.TabPane;
+const TestItemEnum = {
+  QUESTION: 1,
+  CHOICE: 2,
+};
 
 const TestItems = ({testItems, loading, dispatch, location}) => {
-  const {list, pagination} = testItems
+  const {list, pagination} = testItems;
+  const {query = {}, pathname} = location;
+
+  /**
+   * 类型切换
+   * @param key
+   */
+  const handleTabClick = (key) => {
+    dispatch(routerRedux.push({
+      pathname,
+      query: {
+        type: key,
+      },
+    }));
+  };
 
   const listProps = {
     pagination,
     dataSource: list,
     loading: loading.effects['testItems/query'],
     location,
-    onChange:(page)=> {
-
-      const {query = {}, pathname} = location
+    onChange: (page) => {
       dispatch(routerRedux.push({
         pathname,
         query: {
@@ -22,18 +41,38 @@ const TestItems = ({testItems, loading, dispatch, location}) => {
           page: page.current,
           pageSize: page.pageSize,
         },
-      }))
+      }));
     },
-  }
+  };
+
+  const quesitonPops = {
+    type: 1,
+    ...listProps
+  };
+  const selectPops = {
+    type: 2,
+    ...listProps
+  };
   return (
-    <List {...listProps}/>
-  )
-}
+    <div className="content-inner">
+      <Tabs
+        activeKey={query.type === String(TestItemEnum.CHOICE) ? String(TestItemEnum.CHOICE) : String(TestItemEnum.QUESTION)}
+        onTabClick={handleTabClick}>
+        <TabPane tab="问答题" key={String(TestItemEnum.QUESTION)}>
+          <List {...quesitonPops} />
+        </TabPane>
+        <TabPane tab="选择题" key={String(TestItemEnum.CHOICE)}>
+          <List {...selectPops} />
+        </TabPane>
+      </Tabs>
+    </div>
+  );
+};
 TestItems.propTypes = {
   post: PropTypes.object,
   loading: PropTypes.object,
   location: PropTypes.object,
   dispatch: PropTypes.func,
-}
+};
 
 export default connect(({testItems, loading}) => ({testItems, loading}))(TestItems);
