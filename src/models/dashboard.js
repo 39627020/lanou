bd5bbcc2630f5536be0739f6accc62c1f8d10935
MengStar <641510128@qq.com>
2017-07-31 22:42:01 +0800

    搞了个虚假的dashboard

--- src/models/dashboard.js
+++ src/models/dashboard.js
@@ -1,168 +1,168 @@
-import { myCity, queryWeather, query } from '../services/dashboard'
-import { parse } from 'qs'
+import * as dashService from '../services/dashboard';
+import {parse} from 'qs';
 
 // zuimei 摘自 http://www.zuimeitianqi.com/res/js/index.js
 let zuimei = {
-  parseActualData (actual) {
+  parseActualData(actual) {
     let weather = {
       icon: `http://www.zuimeitianqi.com/res/icon/${zuimei.getIconName(actual.wea, 'big')}`,
       name: zuimei.getWeatherName(actual.wea),
       temperature: actual.tmp,
       dateTime: new Date(actual.PTm).format('MM-dd hh:mm'),
-    }
-    return weather
+    };
+    return weather;
   },
 
-  getIconName (wea, flg) {
-    let myDate = new Date()
-    let hour = myDate.getHours()
-    let num = 0
+  getIconName(wea, flg) {
+    let myDate = new Date();
+    let hour = myDate.getHours();
+    let num = 0;
     if (wea.indexOf('/') !== -1) {
-      let weas = wea.split('/')
+      let weas = wea.split('/');
       if (hour < 12) {
-        num = zuimei.replaceIcon(weas[0])
+        num = zuimei.replaceIcon(weas[0]);
         if (num < 6) {
-          num = `${num}_${flg}_night.png`
+          num = `${num}_${flg}_night.png`;
         } else {
-          num = `${num}_${flg}.png`
+          num = `${num}_${flg}.png`;
         }
       } else if (hour >= 12) {
-        num = zuimei.replaceIcon(weas[1])
+        num = zuimei.replaceIcon(weas[1]);
         if (hour >= 18) {
-          num = `${num}_${flg}_night.png`
+          num = `${num}_${flg}_night.png`;
         } else {
-          num = `${num}_${flg}.png`
+          num = `${num}_${flg}.png`;
         }
       }
     } else {
       if ((hour >= 18 && hour <= 23) || (hour >= 0 && hour <= 6)) {
-        num = `${num}_${flg}_night.png`
+        num = `${num}_${flg}_night.png`;
       } else {
-        num = `${num}_${flg}.png`
+        num = `${num}_${flg}.png`;
       }
     }
 
-    return num
+    return num;
   },
 
-  replaceIcon (num) {
+  replaceIcon(num) {
     if (num === 21) {
-      num = 7
+      num = 7;
     } else if (num === 22) {
-      num = 8
+      num = 8;
     } else if (num === 10 || num === 11 || num === 12 || num === 23 || num === 24 || num === 25) {
-      num = 9
+      num = 9;
     } else if (num === 13 || num === 15 || num === 26 || num === 27 || num === 34) {
-      num = 14
+      num = 14;
     } else if (num === 17 || num === 28) {
-      num = 16
+      num = 16;
     } else if (num === 35) {
-      num = 18
+      num = 18;
     } else if (num === 31 || num === 32 || num === 33) {
-      num = 20
+      num = 20;
     } else if (num === 30) {
-      num = 29
+      num = 29;
     }
 
-    return num
+    return num;
   },
 
-  getWeatherName (wea) {
-    let name = ''
+  getWeatherName(wea) {
+    let name = '';
     if (wea.indexOf('/') !== -1) {
-      let weas = wea.split('/')
-      name = `${zuimei.getWeatherByCode(weas[0])}转${zuimei.getWeatherByCode(weas[1])}`
+      let weas = wea.split('/');
+      name = `${zuimei.getWeatherByCode(weas[0])}转${zuimei.getWeatherByCode(weas[1])}`;
     } else {
-      name = zuimei.getWeatherByCode(wea)
+      name = zuimei.getWeatherByCode(wea);
     }
 
-    return name
+    return name;
   },
 
-  getWeatherByCode (number) {
-    let wea = ''
-    let num = Number(number)
+  getWeatherByCode(number) {
+    let wea = '';
+    let num = Number(number);
     if (num === 0) {
-      wea = '晴'
+      wea = '晴';
     } else if (num === 1) {
-      wea = '多云'
+      wea = '多云';
     } else if (num === 2) {
-      wea = '阴'
+      wea = '阴';
     } else if (num === 3) {
-      wea = '阵雨'
+      wea = '阵雨';
     } else if (num === 4) {
-      wea = '雷阵雨'
+      wea = '雷阵雨';
     } else if (num === 5) {
-      wea = '雷阵雨并伴有冰雹'
+      wea = '雷阵雨并伴有冰雹';
     } else if (num === 6) {
-      wea = '雨夹雪'
+      wea = '雨夹雪';
     } else if (num === 7) {
-      wea = '小雨'
+      wea = '小雨';
     } else if (num === 8) {
-      wea = '中雨'
+      wea = '中雨';
     } else if (num === 9) {
-      wea = '大雨'
+      wea = '大雨';
     } else if (num === 10) {
-      wea = '暴雨'
+      wea = '暴雨';
     } else if (num === 11) {
-      wea = '大暴雨'
+      wea = '大暴雨';
     } else if (num === 12) {
-      wea = '特大暴雨'
+      wea = '特大暴雨';
     } else if (num === 13) {
-      wea = '阵雪'
+      wea = '阵雪';
     } else if (num === 14) {
-      wea = '小雪'
+      wea = '小雪';
     } else if (num === 15) {
-      wea = '中雪'
+      wea = '中雪';
     } else if (num === 16) {
-      wea = '大雪'
+      wea = '大雪';
     } else if (num === 17) {
-      wea = '暴雪'
+      wea = '暴雪';
     } else if (num === 18) {
-      wea = '雾'
+      wea = '雾';
     } else if (num === 19) {
-      wea = '冻雨'
+      wea = '冻雨';
     } else if (num === 20) {
-      wea = '沙尘暴'
+      wea = '沙尘暴';
     } else if (num === 21) {
-      wea = '小雨-中雨'
+      wea = '小雨-中雨';
     } else if (num === 22) {
-      wea = '中雨-大雨'
+      wea = '中雨-大雨';
     } else if (num === 23) {
-      wea = '大雨-暴雨'
+      wea = '大雨-暴雨';
     } else if (num === 24) {
-      wea = '暴雨-大暴雨'
+      wea = '暴雨-大暴雨';
     } else if (num === 25) {
-      wea = '大暴雨-特大暴雨'
+      wea = '大暴雨-特大暴雨';
     } else if (num === 26) {
-      wea = '小雪-中雪'
+      wea = '小雪-中雪';
     } else if (num === 27) {
-      wea = '中雪-大雪'
+      wea = '中雪-大雪';
     } else if (num === 28) {
-      wea = '大雪-暴雪'
+      wea = '大雪-暴雪';
     } else if (num === 29) {
-      wea = '浮沉'
+      wea = '浮沉';
     } else if (num === 30) {
-      wea = '扬沙'
+      wea = '扬沙';
     } else if (num === 31) {
-      wea = '强沙尘暴'
+      wea = '强沙尘暴';
     } else if (num === 32) {
-      wea = '飑'
+      wea = '飑';
     } else if (num === 33) {
-      wea = '龙卷风'
+      wea = '龙卷风';
     } else if (num === 34) {
-      wea = '若高吹雪'
+      wea = '若高吹雪';
     } else if (num === 35) {
-      wea = '轻雾'
+      wea = '轻雾';
     } else if (num === 53) {
-      wea = '霾'
+      wea = '霾';
     } else if (num === 99) {
-      wea = '未知'
+      wea = '未知';
     }
 
-    return wea
+    return wea;
   },
-}
+};
 
 export default {
   namespace: 'dashboard',
@@ -189,17 +189,17 @@ export default {
     },
   },
   subscriptions: {
-    setup ({ dispatch }) {
-      dispatch({ type: 'query' })
-      dispatch({ type: 'queryWeather' })
+    setup({dispatch}) {
+      dispatch({type: 'query'});
+      dispatch({type: 'queryWeather'});
     },
   },
   effects: {
-    *query ({
-      payload,
-    }, { call, put }) {
-     // const data = yield call(query, parse(payload))
-      const data ={
+    * query({
+              payload,
+            }, {call, put}) {
+      // const data = yield call(dashService.query, parse(payload))
+      const data = {
         "sales": [{"name": 2008, "Clothes": 273, "Food": 372, "Electronics": 332}, {
           "name": 2009,
           "Clothes": 452,
@@ -426,32 +426,222 @@ export default {
           "number": 253
         }, {"icon": "shopping-cart", "color": "#f69899", "title": "Referrals", "number": 4324}]
       };
-      yield put({ type: 'queryWeather', payload: { ...data } })
+      yield put({type: 'queryWeather', payload: {...data}});
     },
-    *queryWeather ({
-      payload,
-    }, { call, put }) {
-      const myCityResult = yield call(myCity, { flg: 0 })
-      const result = yield call(queryWeather, { cityCode: myCityResult.selectCityCode })
-      const weather = zuimei.parseActualData(result.data.actual)
-      weather.city = myCityResult.selectCityName
-      yield put({ type: 'queryWeatherSuccess', payload: {
-        weather,
-      } })
+    * queryWeather({
+                     payload,
+                   }, {call, put}) {
+      // const myCityResult = yield call(dashService.myCity, { flg: 0 }
+      // const result = yield call(dashService.queryWeather, { cityCode: myCityResult.selectCityCode })
+      const result = {
+        "rtnCode": "0",
+        "rtnMsg": "",
+        "serverDate": "1501483578694",
+        "data": {
+          "cityCode": "01010101",
+          "actual": {
+            "PTm": "2017-07-31 14:34:41",
+            "desc": "天气不错，出去走走。",
+            "descType": "1",
+            "feelT": "32",
+            "ftv": "",
+            "high": "32",
+            "hum": "58",
+            "ldt": "丁酉年六月初九",
+            "low": "23",
+            "pre": "1017",
+            "tmp": "31",
+            "uvIndex": "5",
+            "vis": "6",
+            "wd": "3",
+            "wea": "0",
+            "week": "2",
+            "wp": "1",
+            "ws": "1"
+          },
+          "forecast": [{
+            "date": "2017-07-30 00:00:00",
+            "ftv": "",
+            "high": "30",
+            "ldt": "丁酉年六月初八",
+            "low": "22",
+            "rc": "05:10",
+            "rl": "19:31",
+            "wd": "3",
+            "wea": "0/1",
+            "week": "1",
+            "wp": "1",
+            "yc": "12:14",
+            "yl": "23:37",
+            "yx": "上弦月",
+            "nxy": "7",
+            "dayRainProbability": "4",
+            "nightRainProbability": "6"
+          }, {
+            "date": "2017-07-31 00:00:00",
+            "ftv": "",
+            "high": "32",
+            "ldt": "丁酉年六月初九",
+            "low": "23",
+            "rc": "05:11",
+            "rl": "19:30",
+            "wd": "3",
+            "wea": "0/1",
+            "week": "2",
+            "wp": "1",
+            "yc": "13:12",
+            "yl": "00:08",
+            "yx": "盈亏月",
+            "nxy": "8",
+            "dayRainProbability": "3",
+            "nightRainProbability": "3"
+          }, {
+            "date": "2017-08-01 00:00:00",
+            "ftv": "建军节",
+            "high": "30",
+            "ldt": "丁酉年六月初十",
+            "low": "24",
+            "rc": "05:12",
+            "rl": "19:29",
+            "wd": "3",
+            "wea": "3",
+            "week": "3",
+            "wp": "1",
+            "yc": "14:08",
+            "yl": "00:43",
+            "yx": "盈亏月",
+            "nxy": "9",
+            "dayRainProbability": "56",
+            "nightRainProbability": "80"
+          }, {
+            "date": "2017-08-02 00:00:00",
+            "ftv": "",
+            "high": "28",
+            "ldt": "丁酉年六月十一",
+            "low": "23",
+            "rc": "05:13",
+            "rl": "19:28",
+            "wd": "1",
+            "wea": "3",
+            "week": "4",
+            "wp": "1",
+            "yc": "15:04",
+            "yl": "01:21",
+            "yx": "盈亏月",
+            "nxy": "10",
+            "dayRainProbability": "55",
+            "nightRainProbability": "80"
+          }, {
+            "date": "2017-08-03 00:00:00",
+            "ftv": "",
+            "high": "33",
+            "ldt": "丁酉年六月十二",
+            "low": "25",
+            "rc": "05:14",
+            "rl": "19:27",
+            "wd": "3",
+            "wea": "1",
+            "week": "5",
+            "wp": "1",
+            "yc": "15:57",
+            "yl": "02:03",
+            "yx": "盈亏月",
+            "nxy": "11",
+            "dayRainProbability": "25",
+            "nightRainProbability": "11"
+          }, {
+            "date": "2017-08-04 00:00:00",
+            "ftv": "",
+            "high": "33",
+            "ldt": "丁酉年六月十三",
+            "low": "25",
+            "rc": "05:15",
+            "rl": "19:25",
+            "wd": "3",
+            "wea": "1/0",
+            "week": "6",
+            "wp": "1",
+            "yc": "16:48",
+            "yl": "02:49",
+            "yx": "盈亏月",
+            "nxy": "12",
+            "dayRainProbability": "2",
+            "nightRainProbability": "51"
+          }, {
+            "date": "2017-08-05 00:00:00",
+            "ftv": "",
+            "high": "32",
+            "ldt": "丁酉年六月十四",
+            "low": "24",
+            "rc": "05:16",
+            "rl": "19:24",
+            "wd": "3",
+            "wea": "1",
+            "week": "7",
+            "wp": "1",
+            "yc": "17:37",
+            "yl": "03:40",
+            "yx": "盈亏月",
+            "nxy": "13",
+            "dayRainProbability": "51",
+            "nightRainProbability": "25"
+          }, {
+            "date": "2017-08-06 00:00:00",
+            "ftv": "",
+            "high": "33",
+            "ldt": "丁酉年六月十五",
+            "low": "23",
+            "rc": "05:17",
+            "rl": "19:23",
+            "wd": "1",
+            "wea": "1",
+            "week": "1",
+            "wp": "1",
+            "yc": "18:21",
+            "yl": "04:35",
+            "yx": "盈亏月",
+            "nxy": "14",
+            "dayRainProbability": "11",
+            "nightRainProbability": "25"
+          }],
+          "air": {
+            "PTm": "2017-07-31 14:00:31",
+            "aqi": "132",
+            "aqigrad": "轻度污染",
+            "co": "0.9",
+            "desc": "空气质量轻度污染，放好心态，健康生活！儿童、老年人及心脏病、呼吸疾病者应尽量减少大量户外运动。",
+            "link": "http://update.coolyun.com/2001/weafile/index/resource/20160527/1464334073596.html",
+            "lv": "3",
+            "no2": "22",
+            "o3": "269",
+            "pm10": "99",
+            "pm2": "79",
+            "so2": "2",
+            "suggestion": "儿童、老年人及心脏病、呼吸系统疾病患者应尽量减少体力消耗大的户外活动。"
+          }
+        }
+      };
+      const weather = zuimei.parseActualData(result.data.actual);
+      weather.city = "成都";
+      yield put({
+        type: 'queryWeatherSuccess', payload: {
+          weather,
+        }
+      });
     },
   },
   reducers: {
-    queryWeatherSuccess (state, action) {
+    queryWeatherSuccess(state, action) {
       return {
         ...state,
         ...action.payload,
-      }
+      };
     },
-    queryWeather (state, action) {
+    queryWeather(state, action) {
       return {
         ...state,
         ...action.payload,
-      }
+      };
     },
   },
-}
+};
