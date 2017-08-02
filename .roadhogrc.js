a8a7f7cdc2eada0b861445c0b27234c6588c47f0
MengStar <641510128@qq.com>
2017-08-02 22:10:54 +0800

    修改roadhogrc 代理

--- .roadhogrc.js
+++ .roadhogrc.js
@@ -12,7 +12,8 @@ export default {
   // 接口代理示例
   "proxy": {
     "/api/v1": {
-      "target": "http://localhost:8080",
+      // "target": "http://localhost:8888",
+      "target": "http://119.29.88.16:8888",
       "changeOrigin": false,
       // "pathRewrite": { "^/api/v1" : "/api/v1" }
     },
